a1018eb5fc99571cb3abde04094d1a1e06974834
Afra10 <afra6982@gmail.com>
2025-03-01 20:54:39 +0530

    Added Buttons

---
+++ Assets/level3.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WordGameManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Question
+    {
+        public string word;
+        public string option1;
+        public string option2;
+        public AudioClip audioClip;
+        public string correctAnswer;
+    }
+
+    public Question[] questions;
+    private int currentQuestionIndex = 0;
+
+    public AudioSource audioSource;
+    public Button replayButton;
+    public Button option1Button;
+    public Button option2Button;
+    public TextMeshProUGUI option1Text;
+    public TextMeshProUGUI option2Text;
+    public GameObject correctMessage;
+    public GameObject wrongMessage;
+
+    void Start()
+    {
+        LoadQuestion();
+        replayButton.onClick.AddListener(PlayAudio);
+        option1Button.onClick.AddListener(() => CheckAnswer(option1Text.text));
+        option2Button.onClick.AddListener(() => CheckAnswer(option2Text.text));
+    }
+
+    void LoadQuestion()
+    {
+        if (currentQuestionIndex >= questions.Length)
+        {
+            Debug.Log("Game Over! No more questions.");
+            return;
+        }
+
+        Question currentQuestion = questions[currentQuestionIndex];
+
+        option1Text.text = currentQuestion.option1;
+        option2Text.text = currentQuestion.option2;
+        audioSource.clip = currentQuestion.audioClip;
+
+        correctMessage.SetActive(false);
+        wrongMessage.SetActive(false);
+    }
+
+    void PlayAudio()
+    {
+        audioSource.Play();
+    }
+
+    void CheckAnswer(string selectedAnswer)
+    {
+        Question currentQuestion = questions[currentQuestionIndex];
+
+        if (selectedAnswer == currentQuestion.correctAnswer)
+        {
+            correctMessage.SetActive(true);
+            wrongMessage.SetActive(false);
+        }
+        else
+        {
+            correctMessage.SetActive(false);
+            wrongMessage.SetActive(true);
+        }
+    }
+}
