4ac7c77f9e832b8eace66256e80f88dd615825b0
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-03-07 15:24:19 +0530

    Letter tracing game initial push
    Adding the game after resolving git errors
    changed : Letter matching game - (added System.IO before .Combine keyword in the documents)

---
+++ Assets/Scripts/Tracing Scripts/Main Game Scripts/BackgroundSizeHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundSizeHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ResizeSpriteToScreen();
+    }
+
+    private void ResizeSpriteToScreen()
+    {
+        var sr = this.GetComponent<SpriteRenderer>();
+
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        var width = sr.sprite.bounds.size.x;
+        var height = sr.sprite.bounds.size.y;
+
+        var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        float myX = (float)worldScreenWidth / width;
+        float myY = (float)worldScreenHeight / height;
+
+        transform.localScale = new Vector3(myX, myY, 0);
+    }
+}
