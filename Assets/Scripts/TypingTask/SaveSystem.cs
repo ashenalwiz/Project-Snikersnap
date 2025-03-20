de9c676e7457a5b7a1571019179e9d5de99611c7
Ashen Alwis <129192701+ashenalwiz@users.noreply.github.com>
2025-03-20 16:36:15 +0530

    Task9 Json
    Json file was made to record data.

---
+++ Assets/Scripts/TypingTask/SaveSystem.cs
@@ -0,0 +1,38 @@
+﻿//using UnityEngine;
+//using System.Collections.Generic;
+//using System.IO;
+//using Newtonsoft.Json;
+//using System.Xml;
+
+//public static class SaveSystem
+//{
+//    private static string folderPath = Path.Combine(Application.dataPath, "GameData"); // Assets/GameData
+//    private static string filePath = Path.Combine(folderPath, "UserProgress.json");
+
+//    public static void SaveData(SessionData data)
+    
+//        if (!Directory.Exists(folderPath))
+//        {
+//            Directory.CreateDirectory(folderPath); // Create GameData folder if not exists
+//        }
+
+//        string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+//        File.WriteAllText(filePath, json);
+
+//        Debug.Log("✅ Progress Saved: " + filePath);
+//    }
+
+//    public static SessionData LoadData()
+//    {
+//        if (File.Exists(filePath))
+//        {
+//            string json = File.ReadAllText(filePath);
+//            return JsonConvert.DeserializeObject<SessionData>(json);
+//        }
+//        else
+//        {
+//            Debug.LogWarning("❌ No Save File Found! Creating new data...");
+//            return new SessionData();
+//        }
+//    }
+//}
